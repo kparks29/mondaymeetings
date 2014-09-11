422be8826a1cc33ae41b6ecdd2297eab8dab603c
kparks29 <ken@awesome>
2014-09-11 15:35:24 -0700

    changed email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,6 @@
 class Mailer < ActionMailer::Base
   def kpi_email(meeting)
     @meeting = meeting
-    mail(from: "ocburgermonster@gmail.com", to: "parks.kendrick@gmail.com ", subject: "Employee KPI's")
+    mail(from: "ocburgermonster@gmail.com", to: "kinney.matthewl@gmail.com", subject: "Employee KPI's")
   end
 end
